4b8daba03076fbab1d898677259695b7dfdfa2de
Lukas-LLS <lukas.lorenz.spilka@gmail.com>
2025-11-23 16:44:09 +0100

    Add sudo to docker commands in start.sh

--- whisper/start.sh
+++ whisper/start.sh
@@ -1,3 +1,3 @@
-docker build -t whisper-whisper .
+sudo docker build -t whisper-whisper .
 
-docker run -it --rm -p "5001:5001" whisper-whisper
+sudo docker run -it --rm -p "5001:5001" whisper-whisper
